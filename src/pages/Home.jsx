c0c2ecdac482aa0bc5105ee985fdc94fb61a2c11
Chun Lam <Chun01@hotmail.fr>
2022-02-03 12:38:50 +0100

    Added Packages & Servers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import {Link} from "react-router-dom"
+import apiHandler from "../api/apiHandler";
 
 const Home = () => {
-	return (
-		<div>
-			<h1>Welcome 🏡</h1>
-		</div>
-	);
+  const [servers, setServers] = useState([]);
+
+  useEffect(() => {
+    apiHandler
+      .get("http://localhost:8080/server/")
+      .then((res) => {
+        setServers(res.data.server);
+      })
+      .catch((e) => console.log(e));
+  }, []);
+
+  return (
+    <div>
+      <h1>Welcome 🏡</h1>
+      {servers.map((server) => (
+		<Link to={`/server/${server._id}`} key={server._id}>{server.name}</Link>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/Server.jsx
+++ src/pages/Server.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import apiHandler from "../api/apiHandler";
 
 const Server = () => {
+  const [server, setServer] = useState({});
+  const { id } = useParams();
+
+  useEffect(() => {
+    apiHandler.get(`http://localhost/server/${id}`).then((res) => {
+      setServer(res.data.server);
+    });
+  }, []);
+
+  // Ici faire un formulaire pour le chat
   return <div></div>;
 };
 
