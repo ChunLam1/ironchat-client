e87f535429d0a93ba56a97e92d822d292ce56f12
Tom <eizandev@gmail.com>
2022-02-07 15:05:06 +0100

    working on servers

--- src/pages/Server.jsx
+++ src/pages/Server.jsx
@@ -2,37 +2,39 @@ import React, { useState, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import apiHandler from "../api/apiHandler";
 import useAuth from "../auth/useAuth";
-import "../styles/socketio.css"
+import "../styles/socketio.css";
 
 import { io } from "socket.io-client";
 
 const Server = () => {
-  const [socket, setSocket] = useState(null)
-  const [messages, setMessages] = useState([])
-  const inputEl = useRef("")
-  const serverId = useParams()
-  const { currentUser } = useAuth()
+  const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const inputEl = useRef("");
+  const serverId = useParams();
+  const { currentUser } = useAuth();
 
   useEffect(() => {
-    setSocket(io("http://localhost:4200"))
-  }, [])
+    setSocket(io("http://localhost:4200"));
+  }, []);
 
   useEffect(() => {
     apiHandler
       .get(`http://localhost:8080/server/${serverId.id}/messages`)
-      .then((res) => setMessages(res.data.message))
-      .catch(e => console.error(e))
-  }, [])
+      .then((res) => {
+        setMessages(res.data.messages);
+      })
+      .catch((e) => console.error(e));
+  }, []);
 
   const sendMessage = (e) => {
-    e.preventDefault()
-    const msg = inputEl.current.value
+    e.preventDefault();
+    const msg = inputEl.current.value;
     socket.emit("message", {
       serverId: serverId.id,
       userId: currentUser._id,
-      content: msg
-    })
-  }
+      content: msg,
+    });
+  };
 
   // Ici faire un formulaire pour le chat
   return (
@@ -40,13 +42,16 @@ const Server = () => {
       <h1>Server</h1>
       <ul id="messages">
         {messages.map((msg) => (
-          <li key={msg._id}>{msg.content}</li>
+          <li key={msg._id}>
+            <b>{msg.userId.name} : </b>
+            {msg.content}
+          </li>
         ))}
       </ul>
-    <form  id="form" onSubmit={(e) => sendMessage(e)}>
-      <input id="input" autoComplete="off" ref={inputEl} />
-      <button>Send</button>
-    </form>
+      <form id="form" onSubmit={(e) => sendMessage(e)}>
+        <input id="input" autoComplete="off" ref={inputEl} />
+        <button>Send</button>
+      </form>
     </div>
   );
 };
